73b82bcf53dfe0c2353fce1803efb416444b4c97
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-10 13:54:22 +0300

    adding the dynamic route for the app

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,2 +1,13 @@
 from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('<username>/profile',viiews.user_profile, name='user_profile'),    
+    path('profile/<username>/settinga', views.edit_profile, name='edit'), 
+    path('project/<post>',views.projectView,name='project'),    
+    path('search/',views.search_project,name='search'), 
+]
 
