1b2e2b8253542e1ccd75d52c2eaef17f307593bd
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-10 17:20:50 +0300

    adding the web forms

--- myapp/forms.py
+++ myapp/forms.py
@@ -8,4 +8,30 @@ class SignupForm(UserCreationForm):
     
     class Meta:
         model = User
+        fields = ('email', 'username', 'password')
         
+class PostForm(forms.ModelForm):
+    image =Imagefield(labe='')
+    
+    class Meta:
+        model = Post
+        fields = ('image', 'title', 'description', 'url','technologies')
+        
+class UpdateUserForm(forms.ModelForm):
+      email = models.EmailField(max_length=255,help_text="Email address")
+      
+      class Meta:
+          model = User
+          fields = ('email', 'username')    
+          
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('name', 'location', 'profile_image','bio','email')    
+        
+                
+class RatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ('design', 'usability', 'content')         
+          
