ac525d68d38a92487a99e1e5b273c4704bc0bdd9
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-11 18:41:42 +0300

    adding styling

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('account/',include('django.contrib.auth.urls'), name='account'),
     path('profile/<username>', views.profile, name='profile'),
     path('<username>/profile',views.user_profile, name='user_profile'),    
-    path('profile/<username>/settinga', views.edit_profile, name='edit'), 
+    path('profile/<username>/setting', views.edit_profile, name='edit'), 
     path('project/<post>',views.projectView,name='project'),    
     path('search/',views.search_project,name='search'), 
 ]
