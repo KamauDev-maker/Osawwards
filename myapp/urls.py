c47229fc8119a5485afeb133e188d887dc8260b4
KamauDev-maker <oscarnjenga@gmail.com>
2022-06-12 17:14:56 +0300

    creating api  url route

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,19 @@
 from django.urls import include,path
 from . import views
+from rest_framework import  routers
 
+router = routers.DefaultRouter()
+router.register('users',views.UserViewSet)
+router.register('posts',views.PostViewSet)
+router.register('profile',views.ProfileViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
     path('account/',include('django.contrib.auth.urls'), name='account'),
+    path('api/', include(router.urls)),
     path('profile/<username>', views.profile, name='profile'),
+    path('api-auth/', include('rest_framework.urls')),
     path('<username>/profile',views.user_profile, name='user_profile'),    
     path('profile/<username>/setting', views.edit_profile, name='edit'), 
     path('project/<post>',views.projectView,name='project'),    
